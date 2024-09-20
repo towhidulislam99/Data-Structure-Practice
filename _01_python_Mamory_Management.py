c8677a8a8909faff44b5f12f91b76977e0a0fb52
towhidulislam99 <towhidulislam618@gmail.com>
2024-09-20 21:48:46 +0600

    Memory Releted

---
+++ _01_python_Mamory_Management.py
@@ -0,0 +1,14 @@
+num = 10;
+num1 = 30
+num2 = 10
+num3 = 10
+print(id(num))
+print(id(num1))
+print(id(num2))
+print(id(num3))
+
+print("The Number Value is: {}, {}, {}".format(num, num1, num2))
+
+print(f"The Number Value is: {num}, {num1}, {num2}")
+
+print(r"The Number Value is: {}, {}, {}".format(num, num1, num2))
---
+++ set.py
@@ -0,0 +1,8 @@
+set = {1, 2, 4, 6, (2, 4, 7)}
+print(type(set))
+
+set.add(100);
+print(set)
+
+set.update([10, 20, 100, 1000, 2000])
+print(set)
